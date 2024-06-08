2a999021e55e9b6811e003d4f492d9808505b28a
Vitalii Makoveichuk <makoveychukvitaliy@gmail.com>
2024-06-08 22:59:39 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = ()=>{
+    return <b>hui</b>
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { AppearanceProvider } from "@twa-dev/mark42";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
-root.render(<AppearanceProvider></AppearanceProvider>);
+root.render(<AppearanceProvider><App/></AppearanceProvider>);
